c89ef7e209a978fbe4e41f4c0f728c2719f9d511
nkunic <natasa.kunic@gmail.com>
2022-05-24 16:07:10 +0200

    2 files modified

--- index.js
+++ index.js
@@ -4,24 +4,46 @@ import 'bootstrap/dist/js/bootstrap.min.js';
 import './css/style.css';
 
 // Write Javascript code!
+
 var firstNumberInput = document.getElementById('firstNumberInput');
 var secondNumberInput = document.getElementById('secondNumberInput');
+
+var firstNumberValue;
+var secondNumberValue;
 
 var multiplyBtn = document.getElementById('multiplyBtn');
 var divideBtn = document.getElementById('divideBtn');
 
-var result = document.getElementById('result');
+var resultPlace = document.getElementById('resultPlace');
+var resultValue;
 
 multiplyBtn.addEventListener('click', multiply);
 function multiply() {
   firstNumberValue = firstNumberInput.value;
   secondNumberValue = secondNumberInput.value;
-  result.innerHTML = firstNumberValue * secondNumberValue;
+  resultValue = firstNumberValue * secondNumberValue;
+
+  console.log(firstNumberValue);
+  console.log(secondNumberValue);
+  console.log(resultValue);
+
+  if (firstNumberValue != null && secondNumberValue != null ) {
+    resultPlace.innerText = resultValue;
+  }
+
 }
 
 divideBtn.addEventListener('click', divide);
 function divide() {
   firstNumberValue = firstNumberInput.value;
   secondNumberValue = secondNumberInput.value;
-  result.innerHTML = firstNumberValue / secondNumberValue;
+  resultValue = firstNumberValue / secondNumberValue;
+
+  console.log(firstNumberValue);
+  console.log(secondNumberValue);
+  console.log(resultValue);
+
+  if (firstNumberValue != null && secondNumberValue != null ) {
+    resultPlace.innerText = resultValue;
+  }
 }
